0b811d7dc1d5c6659c07ed7fd05a3b0c04f09d52
Jackson Goerner <37640160+glipR@users.noreply.github.com>
2023-07-27 12:54:27 +1000

    Add team docstrings.

--- team.py
+++ team.py
@@ -82,9 +82,125 @@ class MonsterTeam:
                 raise ValueError("Spawning logic failed.")
 
     def select_manually(self):
+        """
+        Prompt the user for input on selecting the team.
+        Any invalid input should have the code prompt the user again.
+
+        First input: Team size. Single integer
+        For _ in range(team size):
+            Next input: Prompt selection of a Monster class.
+                * Should take a single input, asking for an integer.
+                    This integer corresponds to an index (1-indexed) of the helpers method
+                    get_all_monsters()
+                * If invalid of monster is not spawnable, should ask again.
+
+        Add these monsters to the team in the same order input was provided. Example interaction:
+
+        How many monsters are there? 2
+        MONSTERS Are:
+        1: Flamikin [✔️]
+        2: Infernoth [❌]
+        3: Infernox [❌]
+        4: Aquariuma [✔️]
+        5: Marititan [❌]
+        6: Leviatitan [❌]
+        7: Vineon [✔️]
+        8: Treetower [❌]
+        9: Treemendous [❌]
+        10: Rockodile [✔️]
+        11: Stonemountain [❌]
+        12: Gustwing [✔️]
+        13: Stormeagle [❌]
+        14: Frostbite [✔️]
+        15: Blizzarus [❌]
+        16: Thundrake [✔️]
+        17: Thunderdrake [❌]
+        18: Shadowcat [✔️]
+        19: Nightpanther [❌]
+        20: Mystifly [✔️]
+        21: Telekite [❌]
+        22: Metalhorn [✔️]
+        23: Ironclad [❌]
+        24: Normake [❌]
+        25: Strikeon [✔️]
+        26: Venomcoil [✔️]
+        27: Pythondra [✔️]
+        28: Constriclaw [✔️]
+        29: Shockserpent [✔️]
+        30: Driftsnake [✔️]
+        31: Aquanake [✔️]
+        32: Flameserpent [✔️]
+        33: Leafadder [✔️]
+        34: Iceviper [✔️]
+        35: Rockpython [✔️]
+        36: Soundcobra [✔️]
+        37: Psychosnake [✔️]
+        38: Groundviper [✔️]
+        39: Faeboa [✔️]
+        40: Bugrattler [✔️]
+        41: Darkadder [✔️]
+        Which monster are you spawning? 38
+        MONSTERS Are:
+        1: Flamikin [✔️]
+        2: Infernoth [❌]
+        3: Infernox [❌]
+        4: Aquariuma [✔️]
+        5: Marititan [❌]
+        6: Leviatitan [❌]
+        7: Vineon [✔️]
+        8: Treetower [❌]
+        9: Treemendous [❌]
+        10: Rockodile [✔️]
+        11: Stonemountain [❌]
+        12: Gustwing [✔️]
+        13: Stormeagle [❌]
+        14: Frostbite [✔️]
+        15: Blizzarus [❌]
+        16: Thundrake [✔️]
+        17: Thunderdrake [❌]
+        18: Shadowcat [✔️]
+        19: Nightpanther [❌]
+        20: Mystifly [✔️]
+        21: Telekite [❌]
+        22: Metalhorn [✔️]
+        23: Ironclad [❌]
+        24: Normake [❌]
+        25: Strikeon [✔️]
+        26: Venomcoil [✔️]
+        27: Pythondra [✔️]
+        28: Constriclaw [✔️]
+        29: Shockserpent [✔️]
+        30: Driftsnake [✔️]
+        31: Aquanake [✔️]
+        32: Flameserpent [✔️]
+        33: Leafadder [✔️]
+        34: Iceviper [✔️]
+        35: Rockpython [✔️]
+        36: Soundcobra [✔️]
+        37: Psychosnake [✔️]
+        38: Groundviper [✔️]
+        39: Faeboa [✔️]
+        40: Bugrattler [✔️]
+        41: Darkadder [✔️]
+        Which monster are you spawning? 2
+        This monster cannot be spawned.
+        Which monster are you spawning? 1
+        """
         raise NotImplementedError
 
     def select_provided(self, provided_monsters:Optional[ArrayR[type[MonsterBase]]]=None):
+        """
+        Generates a team based on a list of already provided monster classes.
+
+        While the type hint imples the argument can be none, this method should never be called without the list.
+        Monsters should be added to the team in the same order as the provided array.
+
+        Example input:
+        [Flamikin, Aquariuma, Gustwing] <- These are all classes.
+
+        Example team if in TeamMode.FRONT:
+        [Gustwing Instance, Aquariuma Instance, Flamikin Instance]
+        """
         raise NotImplementedError
 
     def choose_action(self, currently_out: MonsterBase, enemy: MonsterBase) -> Battle.Action:
